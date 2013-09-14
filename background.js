46525d4f4f74f021da0dc5f40e5ffd01958d6a88
pythonwood <582223837@qq.com>
2013-09-14 15:51:36 +0800

    jumper-v3-改回用Object内存问题改善

--- background.js
+++ background.js
@@ -1,31 +1,33 @@
 //{"id":"", "state":0, "count":1}
 //g_dict = new Array()
-//g_dict = {}  //{}是Object,没有length； Array有length
-g_dict = []
+g_dict = {}  //{}是Object,没有length； Array有length
+//g_dict = []
 chrome.extension.onMessage.addListener(function(request, sender, callback) {
     question = request["q"]
     id = request["id"]
     console.log("question: " + question + " id: " + id)
     if (question == "state"){
         //debug test it
-        if(g_dict.length>0){
+//        if(g_dict.length>0){
             callback(g_dict)
-        }else{
-            callback(null)
-        }
-//    }else if (question == "sure"){
-//        if(g_dict[id]){
-//            delete g_dict[id]
-//            callback(ture)
 //        }else{
+//            callback(null)
+//        }
+//下面这种回答其实不必的，所以删除
+//  }else if (question == "sure"){
+//       if(g_dict[id]){
+//           delete g_dict[id]
+//           callback(ture)
+//       }else{
 //            callback(false)
 //        }
     }else if (question == "count"){
         g_dict[id]["count"]++
         callback(g_dict[id]["count"])
     }else if (question == "start"){
-        g_dict[id] = []//g_dict[id] = {"count":0}
-        g_dict[id]["count"] = 0
+	g_dict[id] = {"count":0}
+        //g_dict[id] = []
+        //g_dict[id]["count"] = 0
         callback()
     }else if (question == "stop"){
         delete g_dict[id]
--- click.js
+++ click.js
@@ -2,10 +2,13 @@
     $subm = $("input:submit")
     console.log("jumper-start-ok ~ submit-num: " + $subm.length)
     
+
+    //因这部分需要，将ID全由排班班号改成课程编号。
     if($subm.length == 2){                                  //确认选课页
 	//      $subm[0].click()                            //可能弹窗，因此可能永远不成功，所以只试一次
 	//      $subm[1].click()                            //
-        id = $(this).next("td:first").text()
+        id = $("input:text").val()
+	console.log("sure id: " + id)
         chrome.extension.sendMessage({"q":"state"}, function(dict){                                     //state问题
             //dict = Array(dict)
             for(k in dict){//$.each(dict, function(k, v){
@@ -20,7 +23,7 @@
                     //      if (dict_in[id]["count"] != dict[id]["count"])  $subm[0].click()
                     chrome.extension.sendMessage({"q":"stop", "id":id}, function(){
                         $subm[0].click() 
-                        console.log("sure and delete callback ~ ok")//short quiet well! take it esay! oh yeah
+                        console.log("submit sure and delete callback ~ ok")//short quiet well! take it esay! oh yeah
                     })
                 }
             }//)
@@ -35,18 +38,28 @@
 	//增加功能键
         $course_tr = $("tbody:last:not(:has(input)) tr:not(:first)")
         $course_tr.find("td:first").append("<a href='#' title='start'>&nbsp;&gt;</a>&nbsp;<a href='#' title='stop'>|</a>")
+
+	//调用默认行为得用javascript层的click()
+	//$course_tr.find("td:first").attr("onclick", $course_tr.find("td:first").attr("href"))
+
         //发送消息：state问题
         chrome.extension.sendMessage({"q":"state"}, function(dict){
             //硬骨头
             //dict = (Array)dict;//dict = Array(dict) //diff!     //转换成Array的尝试失败了。!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             //dict = dict.toArray();//TypeError: Object #<Object> has no method 'toArray'
-            console.log("state callback ~ para dict: " + dict + " dict==undefined: " + (dict==undefined) + " typeof: " + typeof(dict) + " dict==null: " + (dict==null) + " dict==false: " + (dict==false))
+	    //打印对象的有用消息
+            //console.log("state callback ~ para dict: " + dict + " dict==undefined: " + (dict==undefined) + " typeof: " + typeof(dict) + " dict==null: " + (dict==null) + " dict==false: " + (dict==false))
+	    //对象为null时会出现异常
             //console.log("dict.length: " + dict.length + " toString: " + dict.toString())
+	    //Array有length，Object无,Array继承自Object，参数传递时退化为Object
             //if(dict.length > 0){
+	    //效果等同于 dict == undefined
             //if(typeof(dict) != undefined){
-	    if (dict==null){}else{
-		console.log("dict not null")
+	    //null也是对象，不等于undefined，javascript很怪异
+	    //if (dict==null){}else{
+		//console.log("dict not null")
 //try{
+	        //each是jQuery封装，用时出现过TypeErr，不放心
 		//$.each(dict, function(k, v){
                 for(k in dict){
 		    //console.log("in $.each() key: " + k)
@@ -56,7 +69,8 @@
                     if ($line.length>0){
                         if( parseInt($line.find("td:eq(10)").text()) - parseInt($line.find("td:eq(11)").text()) > 0 ){
 			    console.log("enought")
-                            $line.find("a:first").click()
+                            $line.find("a:first")[0].click()//必须是javascript层的click才行！！
+			    //console.log("a:first: " +  $line.find("a:first").attr("href"))
                         }else{
 			    console.log("not enought")
                             chrome.extension.sendMessage({"q":"count","id":k}, function(count){     //count问题
@@ -67,16 +81,17 @@
                     }
                 }
 		//})
-            }
+            //}
 //catch(err){console.log(err.what())}
         })
-        console.log("gocenter")
+//        console.log("gocenter")
 
         //消息异步处理，所以会到这里
         //if click "start"    //start问题
         $("a[title=start]").click(function(){
             console.log("start checking")
-            id = $(this).parent().next().text()
+            //id = $(this).parent().next().text()
+	    id = $(this).parent().next().next().text()//由排班班号改成课程编号。
             console.log("send start id: " + id)
             chrome.extension.sendMessage({"q":"start", "id":id}, function(){
                 setTimeout("$subm[5].click()", 2100)
@@ -86,11 +101,12 @@
             console.log("sent message")
             return false
         })
-	console.log("click one")
+//	console.log("click one")
 
         //if click "stop"
         $("a[title=stop]").click(function(){
-            id = $(this).parent().next().text()
+            //id = $(this).parent().next().text()
+	    id = $(this).parent().next().next().text()//由排班班号改成课程编号。
 	    //stop问题
             chrome.extension.sendMessage({"q":"stop", "id":id}, function(){
                 setTimeout("$subm[5].click()", 2100)
@@ -99,18 +115,18 @@
             })         
             return false
         })
-	console.log("click twice")
+//	console.log("click twice")
 	
 	//state问题
         chrome.extension.sendMessage({"q":"state"}, function(dict){                                    
             //if (dict.length==0){
-	    if (dict==null){
-            //if (typeof(dict)==undefined){
+	    //if (dict==null){
+            //if (dict==undefined){
 		//自行刷新保持session,防止自动执行
                 setTimeout(function(){$subm[5].click()}, 90000)                                         
-            }
+            //}
         })
-	console.log("click thrid")
+//	console.log("click thrid")
 
 	//endof:  $subm.length == 6
     }
