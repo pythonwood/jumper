445df6ad26fe4eda1872fdbb0b472596a2bb3959
pythonwood <582223837@qq.com>
2013-12-19 19:44:51 +0800

    for 1.1v

--- background.js
+++ background.js
@@ -1,38 +1,23 @@
 //{"id":"", "state":0, "count":1}
 //g_dict = new Array()
-g_dict = {}  //{}是Object,没有length； Array有length
+g_dict = {}  //{}是Object,没有length； Array有length；还有原因是内存问题，所以用Object
 //g_dict = []
 chrome.extension.onMessage.addListener(function(request, sender, callback) {
     question = request["q"]
     id = request["id"]
-    console.log("question: " + question + " id: " + id)
+    //分别回答state,count,start,stop提问(后来减少了sure，因为可组合曲线之)
     if (question == "state"){
-        //debug test it
-//        if(g_dict.length>0){
             callback(g_dict)
-//        }else{
-//            callback(null)
-//        }
-//下面这种回答其实不必的，所以删除
-//  }else if (question == "sure"){
-//       if(g_dict[id]){
-//           delete g_dict[id]
-//           callback(ture)
-//       }else{
-//            callback(false)
-//        }
     }else if (question == "count"){
         g_dict[id]["count"]++
         callback(g_dict[id]["count"])
     }else if (question == "start"){
-	g_dict[id] = {"count":0}
-        //g_dict[id] = []
-        //g_dict[id]["count"] = 0
+    g_dict[id] = {"count":0}
         callback()
     }else if (question == "stop"){
-        delete g_dict[id]
+        delete g_dict[id]   //后来才知道简单删除状态可矣！
         callback()
-    }else {
+    }else {     //应该不用这个分支
         callback(g_dict)
     }
 });
